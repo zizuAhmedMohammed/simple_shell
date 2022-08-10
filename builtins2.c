8b85bed41b982dbdb7cc79add9acc87d0c85a9a3
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 07:40:55 -0700

    third commit

--- builtins2.c
+++ builtins2.c
@@ -60,3 +60,29 @@ char **find_key(char **env, char *key)
 	}
 	return (NULL);
 }
+
+/**
+ * add_value - create a new environment variable string
+ * @key: variable name
+ * @value: variable value
+ *
+ * Return: pointer to the new string;
+ */
+char *add_value(char *key, char *value)
+{
+	unsigned int len1, len2, i, j;
+	char *new;
+
+	len1 = _strlen(key);
+	len2 = _strlen(value);
+	new = malloc(sizeof(char) * (len1 + len2 + 2));
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; key[i] != '\0'; i++)
+		new[i] = key[i];
+	new[i] = '=';
+	for (j = 0; value[j] != '\0'; j++)
+		new[i + 1 + j] = value[j];
+	new[i + 1 + j] = '\0';
+	return (new);
+}
