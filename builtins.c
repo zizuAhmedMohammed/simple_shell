be9ff457ff0297b6b03de957e9a94020003a4141
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 07:08:39 -0700

    third commit

--- builtins.c
+++ builtins.c
@@ -25,3 +25,34 @@ void (*check_for_builtins(vars_t *vars))(vars_t *vars)
 		check[i].f(vars);
 	return (check[i].f);
 }
+
+/**
+ * new_exit - exit program
+ * @vars: variables
+ * Return: void
+ */
+void new_exit(vars_t *vars)
+{
+	int status;
+
+	if (_strcmpr(vars->av[0], "exit") == 0 && vars->av[1] != NULL)
+	{
+		status = _atoi(vars->av[1]);
+		if (status == -1)
+		{
+			vars->status = 2;
+			print_error(vars, ": Illegal number: ");
+			_puts2(vars->av[1]);
+			_puts2("\n");
+			free(vars->commands);
+			vars->commands = NULL;
+			return;
+		}
+		vars->status = status;
+	}
+	free(vars->buffer);
+	free(vars->av);
+	free(vars->commands);
+	free_env(vars->env);
+	exit(vars->status);
+}
