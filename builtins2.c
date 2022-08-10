ec777a03b0cae8877aec3bcd1cefbc139d552d39
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 07:37:36 -0700

    second commit

--- builtins2.c
+++ builtins2.c
@@ -38,3 +38,25 @@ void add_key(vars_t *vars)
 	vars->env = newenv;
 }
 
+/**
+ * find_key - finds an environment variable
+ * @env: array of environment variables
+ * @key: environment variable to find
+ *
+ * Return: pointer to address of the environment variable
+ */
+char **find_key(char **env, char *key)
+{
+	unsigned int i, j, len;
+
+	len = _strlen(key);
+	for (i = 0; env[i] != NULL; i++)
+	{
+		for (j = 0; j < len; j++)
+			if (key[j] != env[i][j])
+				break;
+		if (j == len && env[i][j] == '=')
+			return (&env[i]);
+	}
+	return (NULL);
+}
