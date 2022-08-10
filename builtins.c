87e6b9ac4fe0373eed3c3691788b6d0d2604ef4f
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 07:12:08 -0700

    fourth commit

--- builtins.c
+++ builtins.c
@@ -56,3 +56,20 @@ void new_exit(vars_t *vars)
 	free_env(vars->env);
 	exit(vars->status);
 }
+
+/**
+ * _env - prints the current environment
+ * @vars: struct of variables
+ * Return: void.
+ */
+void _env(vars_t *vars)
+{
+	unsigned int i;
+
+	for (i = 0; vars->env[i]; i++)
+	{
+		_puts(vars->env[i]);
+		_puts("\n");
+	}
+	vars->status = 0;
+}
