32f5d571a0338859920b8d4ccad76daee8e170b6
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 08:52:57 -0700

    forth commit

--- path.c
+++ path.c
@@ -113,3 +113,50 @@ void check_for_path(vars_t *vars)
 	if (r == 1)
 		new_exit(vars);
 }
+
+/**
+ * execute_cwd - executes the command in the current working directory
+ * @vars: pointer to struct of variables
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int execute_cwd(vars_t *vars)
+{
+	pid_t child_pid;
+	struct stat buf;
+
+	if (stat(vars->av[0], &buf) == 0)
+	{
+		if (access(vars->av[0], X_OK) == 0)
+		{
+			child_pid = fork();
+			if (child_pid == -1)
+				print_error(vars, NULL);
+			if (child_pid == 0)
+			{
+				if (execve(vars->av[0], vars->av, vars->env) == -1)
+					print_error(vars, NULL);
+			}
+			else
+			{
+				wait(&vars->status);
+				if (WIFEXITED(vars->status))
+					vars->status = WEXITSTATUS(vars->status);
+				else if (WIFSIGNALED(vars->status) && WTERMSIG(vars->status) == SIGINT)
+					vars->status = 130;
+				return (0);
+			}
+			vars->status = 127;
+			return (1);
+		}
+		else
+		{
+			print_error(vars, ": Permission denied\n");
+			vars->status = 126;
+		}
+			return (0);
+	}
+	print_error(vars, ": not found\n");
+	vars->status = 127;
+	return (0);
+}
