4cd9023560a54a6829e36978072b52554ed85d2a
Ziz Ahmed <azizazu0934@gmail.com>
2022-08-10 09:57:19 -0700

    String function

--- strfunc.c
+++ strfunc.c
@@ -63,3 +63,59 @@ int _strcmpr(char *strcmp1, char *strcmp2)
 	}
 	return (strcmp1[i] - strcmp2[i]);
 }
+
+/**
+ * _strcat - concatenates two strings
+ * @strc1: first string
+ * @strc2: second string
+ * Return: pointer
+ */
+char *_strcat(char *strc1, char *strc2)
+{
+	char *newstring;
+	unsigned int len1, len2, newlen, i, j;
+
+	len1 = 0;
+	len2 = 0;
+	if (strc1 == NULL)
+		len1 = 0;
+	else
+	{
+		for (len1 = 0; strc1[len1]; len1++)
+			;
+	}
+	if (strc2 == NULL)
+		len2 = 0;
+	else
+	{
+		for (len2 = 0; strc2[len2]; len2++)
+			;
+	}
+	newlen = len1 + len2 + 2;
+	newstring = malloc(newlen * sizeof(char));
+	if (newstring == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		newstring[i] = strc1[i];
+	newstring[i] = '/';
+	for (j = 0; j < len2; j++)
+		newstring[i + 1 + j] = strc2[j];
+	newstring[len1 + len2 + 1] = '\0';
+	return (newstring);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string to be measured
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int len;
+
+	len = 0;
+
+	for (len = 0; str[len]; len++)
+		;
+	return (len);
+}
