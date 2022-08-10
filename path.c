1af908312d70e33f806d862693a10512a32e56b6
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 08:49:18 -0700

    second commit

--- path.c
+++ path.c
@@ -40,3 +40,26 @@ int path_execute(char *command, vars_t *vars)
 	}
 	return (0);
 }
+
+/**
+ * find_path - finds the PATH variable
+ * @env: array of environment variables
+ *
+ * Return: pointer to the node that contains the PATH, or NULL on failure
+ */
+char *find_path(char **env)
+{
+	char *path = "PATH=";
+	unsigned int i, j;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		for (j = 0; j < 5; j++)
+			if (path[j] != env[i][j])
+				break;
+		if (j == 5)
+			break;
+	}
+	return (env[i]);
+
+}
