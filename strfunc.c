53e118d28f65c01bcd9b7c1bb32d4dd3498f877d
Ziz Ahmed <azizazu0934@gmail.com>
2022-08-10 09:34:58 -0700

    String function

--- strfunc.c
+++ strfunc.c
@@ -19,3 +19,27 @@ ssize_t _puts(char *str)
 	}
 	return (len);
 }
+
+/**
+ * _strdup - returns pointer to new mem alloc space which contains copy
+ * @strtodup: string to be duplicated
+ * Return: a pointer to the new duplicated string
+ */
+char *_strdup(char *strtodup)
+{
+	char *copy;
+
+		int len, i;
+
+	if (strtodup == 0)
+		return (NULL);
+
+	for (len = 0; strtodup[len]; len++)
+		;
+	copy = malloc((len + 1) * sizeof(char));
+
+	for (i = 0; i <= len; i++)
+		copy[i] = strtodup[i];
+
+	return (copy);
+}
