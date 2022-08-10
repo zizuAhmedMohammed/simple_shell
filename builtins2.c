75bdeef08468c8dfe5da15502cb795143e66c41e
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 07:42:54 -0700

    builtins2.c

--- builtins2.c
+++ builtins2.c
@@ -86,3 +86,33 @@ char *add_value(char *key, char *value)
 	new[i + 1 + j] = '\0';
 	return (new);
 }
+
+/**
+ * _atoi - converts a string into an integer
+ * @str: string to convert
+ *
+ * Return: the integer value, or -1 if an error occurs
+ */
+int _atoi(char *str)
+{
+	unsigned int i, digits;
+	int num = 0, num_test;
+
+	num_test = INT_MAX;
+	for (digits = 0; num_test != 0; digits++)
+		num_test /= 10;
+	for (i = 0; str[i] != '\0' && i < digits; i++)
+	{
+		num *= 10;
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+		if ((i == digits - 1) && (str[i] - '0' > INT_MAX % 10))
+			return (-1);
+		num += str[i] - '0';
+		if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
+			return (-1);
+	}
+	if (i > digits)
+		return (-1);
+	return (num);
+}
