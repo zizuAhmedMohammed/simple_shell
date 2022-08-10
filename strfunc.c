d70e6209238e0e69807f2011734dd35449314d6c
Ziz Ahmed <azizazu0934@gmail.com>
2022-08-10 09:43:32 -0700

    String function

--- strfunc.c
+++ strfunc.c
@@ -43,3 +43,23 @@ char *_strdup(char *strtodup)
 
 	return (copy);
 }
+
+/**
+ * _strcmpr - compares two strings
+ * @strcmp1: first string, of two, to be compared in length
+ * @strcmp2: second string, of two, to be compared
+ * Return: 0 on success, anything else is a failure
+ */
+int _strcmpr(char *strcmp1, char *strcmp2)
+{
+	int i;
+
+	i = 0;
+	while (strcmp1[i] == strcmp2[i])
+	{
+		if (strcmp1[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (strcmp1[i] - strcmp2[i]);
+}
