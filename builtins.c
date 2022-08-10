7716edea2ef8423fd64b08f513c59b2bca242958
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 07:16:40 -0700

    fifth commit

--- builtins.c
+++ builtins.c
@@ -73,3 +73,41 @@ void _env(vars_t *vars)
 	}
 	vars->status = 0;
 }
+
+/**
+ * new_setenv - create a new environment variable, or edit an existing variable
+ * @vars: pointer to struct of variables
+ *
+ * Return: void
+ */
+void new_setenv(vars_t *vars)
+{
+	char **key;
+	char *var;
+
+	if (vars->av[1] == NULL || vars->av[2] == NULL)
+	{
+		print_error(vars, ": Incorrect number of arguments\n");
+		vars->status = 2;
+		return;
+	}
+	key = find_key(vars->env, vars->av[1]);
+	if (key == NULL)
+		add_key(vars);
+	else
+	{
+		var = add_value(vars->av[1], vars->av[2]);
+		if (var == NULL)
+		{
+			print_error(vars, NULL);
+			free(vars->buffer);
+			free(vars->commands);
+			free(vars->av);
+			free_env(vars->env);
+			exit(127);
+		}
+		free(*key);
+		*key = var;
+	}
+	vars->status = 0;
+}
