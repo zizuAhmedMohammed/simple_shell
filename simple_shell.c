399a501d83772a836d51f7f628dde23e065eda14
Ziz Ahmed <azizazu0934@gmail.com>
2022-08-10 10:08:37 -0700

    Modify simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
 /* global variable for ^C handling */
+unsigned int sig_flag;
 
 /**
  * sig_handler - handles ^C signal interupt
@@ -10,7 +11,6 @@
  */
 static void sig_handler(int uuv)
 {
-	unsigned int sig_flag;
 	(void) uuv;
 	if (sig_flag == 0)
 		_puts("\n$ ");
@@ -28,7 +28,6 @@ static void sig_handler(int uuv)
  */
 int main(int argc __attribute__((unused)), char **argv, char **environment)
 {
-	unsigned int sig_flag;
 	size_t len_buffer = 0;
 	unsigned int is_pipe = 0, i;
 	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, NULL};
