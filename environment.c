f4246f6c948f6966acf4983ef91f6cba15863fae
Eyerusalem <jerry.gigar@gmail.com>
2022-08-10 07:53:34 -0700

    environment.c

--- environment.c
+++ environment.c
@@ -24,3 +24,18 @@ char **make_env(char **env)
 	newenv[i] = NULL;
 	return (newenv);
 }
+
+/**
+ * free_env - free the shell's environment
+ * @env: shell's environment
+ *
+ * Return: void
+ */
+void free_env(char **env)
+{
+	unsigned int i;
+
+	for (i = 0; env[i] != NULL; i++)
+		free(env[i]);
+	free(env);
+}
